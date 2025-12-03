26f518509a09da64905726c8ca58dd62c6f85a4b
khw11044 <khw11044@naver.com>
2025-12-03 15:53:29 +0900

    FEAT : Main Header Compoent

---
+++ js/components/main-header.js
@@ -0,0 +1,24 @@
+class MainHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <header class="main-header">
+        <div class="header-container">
+          <div class="logo">
+            <img src="img/logo.png" alt="RobesEasy Logo">
+          </div>
+          <nav class="main-nav">
+            <ul>
+              <li><a href="index.html">Home</a></li>
+              <li><a href="documents.html">Documents</a></li>
+              <li><a href="products.html">Products</a></li>
+              <li><a href="education.html">Education</a></li>
+              <li><a href="news.html">News</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    `;
+  }
+}
+
+customElements.define('main-header', MainHeader);
