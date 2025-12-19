e795ea8134b02fd13769c865d5e6d4ec419ff779
GomSon-E <gomsonixx@gmail.com>
2025-12-19 19:49:41 +0900

    REFACTOR : Apply BEM Naming Convention across Codebase

--- js/components/main-header.js
+++ js/components/main-header.js
@@ -1,18 +1,18 @@
 class MainHeader extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <header class="main-header">
-        <div class="header-container">
-          <div class="logo">
-            <img src="img/logo.png" alt="RobesEasy Logo">
+      <header class="header">
+        <div class="header__container">
+          <div class="header__logo">
+            <img class="header__logo-image" src="img/logo.png" alt="RobesEasy Logo">
           </div>
-          <nav class="main-nav">
-            <ul>
-              <li><a href="index.html">Home</a></li>
-              <li><a href="documents.html">Documents</a></li>
-              <li><a href="products.html">Products</a></li>
-              <li><a target="_blank" href="https://forms.gle/r1A5JYj4dYZQJNoq8">Education</a></li>
-              <li><a href="news.html">News</a></li>
+          <nav class="header__nav">
+            <ul class="header__nav-list">
+              <li><a class="header__nav-link" href="index.html">Home</a></li>
+              <li><a class="header__nav-link" href="documents.html">Documents</a></li>
+              <li><a class="header__nav-link" href="products.html">Products</a></li>
+              <li><a class="header__nav-link" target="_blank" href="https://forms.gle/r1A5JYj4dYZQJNoq8">Education</a></li>
+              <li><a class="header__nav-link" href="news.html">News</a></li>
             </ul>
           </nav>
         </div>
